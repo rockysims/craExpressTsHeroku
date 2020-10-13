bb7e8627c1d0545015e04588ee90af87e6421ad5
rockysims <rocky.sims@gmail.com>
2020-10-12 23:56:43 -0700

    Changed client result shown by App to match format of server result.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -22,7 +22,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Client result {sum(1, 3)}</p>
+				<p>Client result {JSON.stringify({val: sum(1, 3)})}</p>
 				<p>Server result {JSON.stringify(serverResult)}</p>
 			</header>
 		</div>
